74cb12085ae68527d28df1fb16c1a23ccfea773e
Mr Magpie <joe.marrone@outlook.com>
2021-01-08 18:38:17 -0600

    starting app

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,39 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+//reference table body
+var tbody = d3.select("tbody");
+
+//Loop through data and console log each sighting
+data.forEach(function(ufoSighting) {
+    console.log(ufoSighting);
+});
+
+//append one table row for each report object
+data.forEach(function(ufoSighting) {
+    console.log(ufoSighting);
+    var row = tbody.append("tr");
+});
+
+//Console log each sighting
+data.forEach(function(ufoSighting) {
+    console.log(ufoSighting);
+    var row = tbody.append("tr");
+
+    Object.entries(ufoSighting).forEach(function([key, value]) {
+        console.log(key, value);
+    
+    });
+});
+
+//append 1 cell per sighting value
+data.forEach(function(ufoSighting) {
+    console.log(ufoSighting);
+    var row = tbody.append("tr");
+
+    Object.entries(ufoSighting).forEach(function([key, value]) {
+        console.log(key, value);
+        var cell = row.append("td");
+    });
+});
